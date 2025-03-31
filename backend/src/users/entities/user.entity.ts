29a83291831061a7595c70db973966000a30b60e
Vlad Yakusevych <vladyakusevich@gmail.com>
2025-03-31 14:44:45 +0300

    feat: added user table

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar', length: 30, name: 'first_name', nullable: false })
+  first_name: string;
+
+  @Column({ type: 'varchar', length: 30, name: 'last_name', nullable: false })
+  last_name: string;
+
+  @Column({ type: 'varchar', length: 50, name: 'email', nullable: false, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255, name: 'password', nullable: false })
+  password: string;
+
+  @Column({ type: 'enum', enum: ['Admin', 'User'], enumName: 'UserRole', default: 'User' })
+  role: 'Admin' | 'User';
+}

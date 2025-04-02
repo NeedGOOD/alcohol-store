3b0454ccb2aeb842e8bdf22a02c7bd9d0bbeadc4
Vlad Yakusevych <vladyakusevich@gmail.com>
2025-04-02 23:05:12 +0300

    feat: no-cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // app.enableCors({
+  //   origin: '*',  // Дозволяє доступ з усіх доменів
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  // Дозволяє ці методи
+  //   allowedHeaders: 'Content-Type, Authorization',  // Дозволяє ці заголовки
+  // });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
